c87341e7fc314995fbaa4502583518e8f190adec
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-08-26 09:14:22 +0100

    fix: address layout issues in footer and CTA sections, improve responsiveness, and optimize video modal behavior

---
+++ hooks/use-mobile.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export const useMobile = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth < 768);
+    }, []);
+
+    return isMobile;
+}
