9fbad6e838fa4c07a928b42f28a0a1e11042c577
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-08-26 14:36:01 +0100

    feat: implement GSAP animations in Hero section for enhanced scroll behavior and register ScrollTrigger in landing page

--- app/landing/index.tsx
+++ app/landing/index.tsx
@@ -7,9 +7,11 @@ import { HeroSection } from '~/components/sections/hero';
 import { HowSlidesSection } from '~/components/sections/how-slides';
 import { PreModelsSection } from '~/components/sections/pre-models';
 import { SlidesSection } from '~/components/sections/slides';
+import {gsap} from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const LandingPage = () => {
-
+    gsap.registerPlugin(ScrollTrigger)
   return (
     <div style={{ ["--bg" as any]: "var(--black)", ["--fg" as any]: "var(--white)" }}>
       <main style={{backgroundColor:"var(--bg)",color:"var(--fg)"}}>
