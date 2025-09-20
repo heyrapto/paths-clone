d55245d2aca9ab4a04aebcccbe186ed3f470a6ed
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-09-20 12:19:44 +0100

    chore: disable server-side rendering in router config and format gsap import in how-it-works component

--- app/components/sections/how-it-works.tsx
+++ app/components/sections/how-it-works.tsx
@@ -1,7 +1,7 @@
 import { slides } from "~/constants";
 import { useMobile } from "../../hooks/use-mobile";
 import { useRef, useEffect } from "react";
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
